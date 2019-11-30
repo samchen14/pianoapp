3174f917441d444dbaea803ec428852c268cee11
samchenMP0 <slc3@illinois.edu>
2019-11-30 12:10:09 -0600

    Initial commit

---
+++ app/src/main/java/com/example/pianoapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.pianoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // horizontal orientation
+        setContentView(R.layout.activity_main);
+    }
+}

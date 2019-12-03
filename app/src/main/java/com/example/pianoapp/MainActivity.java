6a852a4c253c757986d70a6e346d9e2517958ba1
evayl2 <evayl2@illinois.edu>
2019-12-03 01:51:41 -0600

    buttons galore!

--- app/src/main/java/com/example/pianoapp/MainActivity.java
+++ app/src/main/java/com/example/pianoapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // horizontal orientation
-        setContentView(R.layout.keyboard);
+        setContentView(R.layout.activity_main);
+        Button play = findViewById(R.id.piano);
+        Button theory = findViewById(R.id.theory);
+        Button settings = findViewById(R.id.setting);
+        Button close = findViewById(R.id.quit);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        theory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void playPiano() {
+
     }
 }

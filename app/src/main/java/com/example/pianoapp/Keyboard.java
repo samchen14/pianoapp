d784061458c31ece40ef1b839599fdfc77d6fd47
samchenMP0 <slc3@illinois.edu>
2019-12-05 09:38:01 -0600

    keys first F to B

--- app/src/main/java/com/example/pianoapp/Keyboard.java
+++ app/src/main/java/com/example/pianoapp/Keyboard.java
@@ -13,7 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Keyboard extends AppCompatActivity {
     private SoundPool soundPool;
-    private int keyMidC, keyMidCSharp, keyMidD, keyMidDSharp, keyMidE, keyMidESharp, keyMidF, keyMidG, keyA;
+    private int keyMidC, keyMidCSharp, keyMidD, keyMidDSharp, keyMidE, keyMidF, keyMidFSharp, keyMidG, keyMidGSharp, keyMidA, keyMidASharp, keyMidB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,12 +64,23 @@ public class Keyboard extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             soundPool = new SoundPool.Builder().setMaxStreams(5).build();
         }
+        // Does setMaxStreams need to match the total number of keys?
+
+
 
         keyMidC = soundPool.load(this, R.raw.midc, 1);
         keyMidCSharp = soundPool.load(this, R.raw.midcsharp, 1);
         keyMidD = soundPool.load(this, R.raw.midd, 1);
         keyMidDSharp = soundPool.load(this, R.raw.middsharp, 1);
         keyMidE = soundPool.load(this, R.raw.mide, 1);
+
+        keyMidF = soundPool.load(this, R.raw.midf, 1);
+        keyMidFSharp = soundPool.load(this, R.raw.midfsharp,1);
+        keyMidG = soundPool.load(this, R.raw.midg, 1);
+        keyMidGSharp = soundPool.load(this, R.raw.midgsharp, 1);
+        keyMidA = soundPool.load(this, R.raw.mida, 1);
+        keyMidASharp = soundPool.load(this, R.raw.midasharp, 1);
+        keyMidB = soundPool.load(this, R.raw.midb, 1);
 
         firstc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -116,6 +127,76 @@ public class Keyboard extends AppCompatActivity {
             public void onClick(View view) {
                 firste.setBackgroundColor(intent.getIntExtra("sColor", Color.BLUE));
                 soundPool.play(keyMidE, 1, 1,
+                        0, 0, 1);
+                //midC.setBackgroundColor(intent.getIntExtra("wColor", Color.WHITE));
+            }
+        });
+
+        firstf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstf.setBackgroundColor(intent.getIntExtra("sColor", Color.BLUE));
+                soundPool.play(keyMidF, 1, 1,
+                        0, 0, 1);
+                //midC.setBackgroundColor(intent.getIntExtra("wColor", Color.WHITE));
+            }
+        });
+
+        firstfsharp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstfsharp.setBackgroundColor(intent.getIntExtra("sColor", Color.BLUE));
+                soundPool.play(keyMidFSharp, 1, 1,
+                        0, 0, 1);
+                //midC.setBackgroundColor(intent.getIntExtra("wColor", Color.WHITE));
+            }
+        });
+
+        firstg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstg.setBackgroundColor(intent.getIntExtra("sColor", Color.BLUE));
+                soundPool.play(keyMidG, 1, 1,
+                        0, 0, 1);
+                //midC.setBackgroundColor(intent.getIntExtra("wColor", Color.WHITE));
+            }
+        });
+
+        firstgsharp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstgsharp.setBackgroundColor(intent.getIntExtra("sColor", Color.BLUE));
+                soundPool.play(keyMidGSharp, 1, 1,
+                        0, 0, 1);
+                //midC.setBackgroundColor(intent.getIntExtra("wColor", Color.WHITE));
+            }
+        });
+
+        firsta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firsta.setBackgroundColor(intent.getIntExtra("sColor", Color.BLUE));
+                soundPool.play(keyMidA, 1, 1,
+                        0, 0, 1);
+                //midC.setBackgroundColor(intent.getIntExtra("wColor", Color.WHITE));
+            }
+        });
+
+        firstasharp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstasharp.setBackgroundColor(intent.getIntExtra("sColor", Color.BLUE));
+                soundPool.play(keyMidASharp, 1, 1,
+                        0, 0, 1);
+                //midC.setBackgroundColor(intent.getIntExtra("wColor", Color.WHITE));
+            }
+        });
+
+        firstb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstb.setBackgroundColor(intent.getIntExtra("sColor", Color.BLUE));
+                soundPool.play(keyMidB, 1, 1,
                         0, 0, 1);
                 //midC.setBackgroundColor(intent.getIntExtra("wColor", Color.WHITE));
             }
